5d2697bf78486a7d3dbf6226d9e1bc197abab417
Elton Pereira de Lima <eltonplima@gmail.com>
2013-12-05 23:10:48 -0300

    Core tests.

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,14 @@
+# coding: utf-8
 from django.test import TestCase
+class HomeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
 
-# Create your tests here.
+    def test_get(self):
+        """
+        GET / must return status code 200.
+        """
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'index.html')
